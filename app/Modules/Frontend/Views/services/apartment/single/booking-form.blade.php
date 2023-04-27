9b6dd949ba8bed3cb1919fa3f25a81c993eabfcd
Enock MAMBOU <enockmambou@gmail.com>
2023-04-27 14:21:48 +0000

    appartement booking form

--- app/Modules/Frontend/Views/services/apartment/single/booking-form.blade.php
+++ app/Modules/Frontend/Views/services/apartment/single/booking-form.blade.php
@@ -73,7 +73,7 @@ $external_booking = $post['external_booking'];
 
             <div class="booking-quantity">
                 <div class="label">
-                    {{__('Adults')}}
+                    Nombre de personne
                 </div>
                 <div class="value">
                     <select class="form-control" name="adult">
@@ -88,36 +88,6 @@ $external_booking = $post['external_booking'];
                     </select>
                 </div>
             </div>
-
-                <div class="booking-quantity">
-                    <div class="label">
-                        {{__('Children')}}
-                    </div>
-                    <div class="value">
-                        <select class="form-control" name="children">
-                            @if($guests > 0)
-                                @for($i = 0; $i <= $guests; $i++)
-                                    <option value="{{$i}}">{{$i}}</option>
-                                @endfor
-                            @endif
-                        </select>
-                    </div>
-                </div>
-
-                <div class="booking-quantity">
-                    <div class="label">
-                        {{__('Infants')}}
-                    </div>
-                    <div class="value">
-                        <select class="form-control" name="infant">
-                            @if($guests > 0)
-                                @for($i = 0; $i <= $guests; $i++)
-                                    <option value="{{$i}}">{{$i}}</option>
-                                @endfor
-                            @endif
-                        </select>
-                    </div>
-                </div>
             @php
                 $extras = maybe_unserialize($post['extra_services']);
             @endphp
