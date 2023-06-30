e0173efe85048c0be5f3424176c3a647aa84cf2c
Enock Mcphen <enockmambou@gmail.com>
2023-06-30 16:44:33 +0000

    TEST MAIL PARTENAIRE send return message bug fix 3 (mail send)

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -113,7 +113,9 @@ class UserService extends AbstractService
         if ($inserted) {
             $admin_id = get_option('admin_user');
             \GMZ_Notification::inst()->addNew($admin_id, $admin_id, __('New Partner request'), __('New Partner request on ') . date(get_date_format()));
-            //dispatch(new SendPartnerRequestJob($post_data));
+
+
+            dispatch(new SendPartnerRequestJob($post_data));
 
             return [
                 'status' => 1,
