a1197e13da1e3a2c3d924c952dd4dbcab4cea3f8
enock <enockmambou@gmail.com>
2024-03-30 08:59:33 +0000

    BOOKING CARROUSSEL 3

--- app/Modules/Frontend/Controllers/OrderController.php
+++ app/Modules/Frontend/Controllers/OrderController.php
@@ -89,7 +89,7 @@ class OrderController extends Controller
             'address'=>$request['address'],
             'city'=>'dakar',
             'country'=>'SN',
-            'phone'=>$request['phone'],
+           // 'phone'=>$request['phone'],
             'email'=>$request['email'],
         ];
         $token = create_token_dpo($datas);// echo $token;dd($dataRequest,$datas);
